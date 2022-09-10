82784762b55c127b2038df49f2c56b3f636dddff
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-09-10 09:38:52 +0300

    mobile menu done

--- header.php
+++ header.php
@@ -10,12 +10,20 @@
 </head>
 <body>
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <?php wp_nav_menu([
-                'theme_location' => 'menu',
-                'container' => 'div',
-                'container_class' => 'container-fluid',
-                'menu_class' => 'navbar',
-                'menu_id' => 'my_top_menu'
-                ]); ?>
-        </nav>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <?php wp_nav_menu([
+                    'theme_location' => 'menu',
+                    'container' => 'div',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id'    => 'navbarNav',
+                    'menu_class' => 'navbar-nav',
+                    'menu_id' => 'my_top_menu'
+                    ]); ?>
+                </div>
+            </nav>
+        </div>
